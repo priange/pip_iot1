d2ff7efe3ede63fbea7893e3c6f39de4e4857d48
priange <124135010+priange@users.noreply.github.com>
2023-04-22 21:22:05 +0300

    Add files via upload
    First commit

---
+++ pip_iot1.ino
@@ -0,0 +1,15 @@
+#include <Servo.h>
+Servo Servo1;
+int ServoPin=9;
+int potPin=A0;
+
+void setup(){
+  Servo1.attach(ServoPin) ;
+}
+
+void loop(){
+ int reading=analogRead(potPin); 
+ int angle=map(reading,0,1023,0,180);
+ Servo1.write(angle);
+  
+}
